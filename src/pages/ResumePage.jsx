3fea2076be4539ef8c222e3c3972ea951906d5cf
Mariana Correa <mpcs270391@gmail.com>
2022-10-26 15:28:07 -0300

    Add ResumePage structure

---
+++ src/pages/ResumePage.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const ResumePage = () => {
+  return (
+    <main>
+        <h2>Your travel information</h2>
+        <section>
+            <p>Origin = XXX</p>
+            <p>Destination = YYY</p>
+            <p>Distance = YYY</p>
+            <p>X Adults, X Children</p>
+            <p>Flight Class = YYY</p>
+            <p>R$ ZZZZ per adult</p>
+            <p>R$ ZZZZ per children</p>
+            <p>Miles: Z</p>
+            <p>Discounted value per miles: R$ ZZZZ </p>
+            <p>Total: R$ ZZZZ</p>
+        </section>
+    </main>
+  )
+}
+
+export default ResumePage
